dbedb35cbf1ff4c15bb4ebd3fd0f0abd71813fee
Andre Luis <andre.1357.luis@gmail.com>
2022-07-08 20:19:54 -0300

    🎉 init

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'google',
+    'prettier',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+    'new-cap': 'off',
+    'no-invalid-this': 'off',
+    'require-jsdoc': 'off',
+    'valid-jsdoc': 'off',
+  },
+};
