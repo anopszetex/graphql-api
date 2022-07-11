87dd1ebbf2f801138bbbfd7bca49d8d4319e5cb1
Andre Luis <andre.1357.luis@gmail.com>
2022-07-10 22:53:01 -0300

    🔨 update script

--- bench-fast.sh
+++ bench-fast.sh
@@ -1,4 +1,4 @@
-URL='http://localhost:4500/graphql'
+URL='http://127.0.0.1:3718/graphql'
 npx autocannon $URL -m POST \
   --headers 'Content-Type: application/json' \
   --body '{"query":"query {\n\tuser(id: 1) {\n\t\tid\n\t\tname\n\t\temail\n\t}\n}"}' \
